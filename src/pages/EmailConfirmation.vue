c1144d4de7a06d7efb35f42089406f8c51d7eb2e
Isac Jossefa Matusse <isacj163@gmail.com>
2022-11-18 07:26:22 -0800

    primeiro commit

---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class= padding>
+    <p class="text-body1">
+      Obrigado por se resgistar !
+    </p>
+    <p class="text-body1">
+      Por favor confima o email para finalizar o resgistro: <strong>{{ $route.query.email }}</strong>
+    </p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
